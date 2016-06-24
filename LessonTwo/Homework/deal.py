94310acf6c0090faf1cf5cf1fa9f7f23de884c5a
Richard <richard.l.huynh@intel.com>
2016-06-24 10:42:26 -0500

    added homework 2 for lesson 2

---
+++ LessonTwo/Homework/deal.py
@@ -0,0 +1,14 @@
+"""
+	Author:
+	Date Created: 6/24/2016
+	Assignment Code: 2-2
+	Assignment Name: Let's Make a Deal
+	Description: please fill this :)
+"""
+
+# google python random module to learn more about this module!
+import random
+
+def solution():
+
+solution()
