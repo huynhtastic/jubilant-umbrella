ebe2b79a92f53f48600189bfd86b467dbcba91f3
Richard Huynh <huynhtastic@gmail.com>
2016-07-06 22:26:59 -0500

    Added shop.py starter code for 3-2

---
+++ LessonThree/Homework/shop.py
@@ -0,0 +1,34 @@
+"""
+    Author: 
+    Date Created: 
+    Assignment Code: 
+    Assignment Name: 
+    Description: 
+
+"""
+
+items = {
+    'apple': 0.99,
+    'banana': 0.50,
+    'orange': 1.00,
+    'steak': 5.00,
+    'chicken': 3.25,
+    'beans': 2.45,
+    'milk': 2.99,
+    'dozen eggs': 1.67,
+    'frozen pizza': 4.54,
+    'UnderWatch Root Edition': 49.99
+}
+
+discounts = {
+    'apple': 4,
+    'banana': 9,
+    'orange': 5,
+    'steak': 4,
+    'chicken': 5,
+    'beans': 5,
+    'milk': 2
+    'dozen eggs': 3,
+    'frozen pizza': 10,
+    'UnderWatch Root Edition': 3
+}
